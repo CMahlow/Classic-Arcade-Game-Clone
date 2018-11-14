9195bf848281c94453efc11f552a5f3f55d3955c
Coding Mommy <christymahlow@gmail.com>
2018-11-13 23:45:21 -0800

    Add this.movement * dt

--- js/app.js
+++ js/app.js
@@ -19,11 +19,11 @@ class Enemy {
 // Parameter: dt, a time delta between ticks
 
 update(dt) {
-Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    this.x += this.movement * dt
+    livesLeft.innerText = lives;
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
