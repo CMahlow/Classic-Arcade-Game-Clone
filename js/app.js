17423c499388c82e564e4684c18cf9a4ee6c8c36
Coding Mommy <christymahlow@gmail.com>
2018-11-13 23:59:53 -0800

    Add collisons code

--- js/app.js
+++ js/app.js
@@ -32,7 +32,25 @@ update(dt) {
 			//New Feature (levels): *400-600 easy *700+ for hard
 			this.movement = 150 + Math.floor(Math.random() * 800);
 		}
-
+    // Collision detection and put the player start from bottom
+    		if (player.x < this.x + 60 &&
+    			player.x + 37 > this.x &&
+    			player.y < this.y + 25 &&
+    			30 + player.y > this.y) {
+    			player.x = 200;
+    			player.y = 400;
+    			lives--;
+    			livesLeft.innerText = lives;
+    			if (lives === 0) {
+    				//Game Over message
+    				confirm(`Game Over! Do you want to play again?`);
+    				lives = 3;
+    				gameScore = 0;
+    				livesLeft.innerText = lives;
+    				score.innerText = '';
+    			}
+    		}
+    	};
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
